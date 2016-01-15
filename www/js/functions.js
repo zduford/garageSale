ef7cc4f473019aa6080fce3d11d4fa74e4a0d9f9
zduford <z.duford@gmail.com>
2016-01-15 12:01:51 -0500

    Initial upload

---
+++ www/js/functions.js
@@ -0,0 +1,13 @@
+// page navigation functionality
+function callMapPage()
+{
+    window.location = "HTML/mapPage.html";
+}
+function callProfilePage()
+{
+    window.location = "HTML/profilePage.html";
+}
+function callAddSalesPage()
+{
+    window.location = "HTML/addSalesPage.html";
+}
